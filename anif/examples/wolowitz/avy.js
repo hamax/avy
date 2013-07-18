5b2575132125f579a9c52b3c49e7528a2bc8d8df
Ziga Ham <ziga@hamsworld.net>
2013-07-18 04:26:51 +0200

    Fix wolowitz example

--- anif/examples/wolowitz/avy.js
+++ anif/examples/wolowitz/avy.js
@@ -1,7 +1,7 @@
-define(['m/ziga/core/playback', 'm/ziga/core/svg', 'm/ziga/graph/force', 'm/ziga/node/simple', 'm/ziga/link/simple'],
+define(['m/ziga/core/playback', 'm/ziga/core/svg', 'm/ziga/graph/force', 'm/ziga/node/simple', 'm/ziga/link/arrow'],
 		function(playback, svg, graph, nodeSimple, linkSimple) {
 	var v = {};
-	v.graph = new graph(svg, new nodeSimple(), new linkSimple());
+	v.graph = new graph(svg, new nodeSimple(undefined, 'value'), new linkSimple());
 
 	playback.load(v);
 });
