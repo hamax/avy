79e9cc5e4f320d9a91d20e244051472e7009d21b
Ziga Ham <ziga@hamsworld.net>
2013-07-20 01:45:21 +0200

    Static homepage

--- client/js/homepage.js
+++ client/js/homepage.js
@@ -1,8 +1,16 @@
 app.controller('HomepageCtrl', function($scope) {
-	$scope.slides = [];
-	for (var i = 0; i < 5; i++) {
-		$scope.slides.push({
-			image: 'http://placekitten.com/' + (500 + Math.round(Math.random() * 300)) + '/300',
-		});
-	}
+	$scope.slides = [
+		{
+			image: '/img/h1.png',
+			link: '/visualizations/ag1zfmF2eS1wcm9qZWN0cjYLEgdhY2NvdW50IhUxMDM3ODgyMzc1NzY2NDI2NjY2NjYMCxINdmlzdWFsaXphdGlvbhjRDww/',
+			caption: 'Topological sorting',
+			padding: 0
+		},
+		{
+			image: '/img/h2.png',
+			link: '/visualizations/ag1zfmF2eS1wcm9qZWN0cjYLEgdhY2NvdW50IhUxMDM3ODgyMzc1NzY2NDI2NjY2NjYMCxINdmlzdWFsaXphdGlvbhjpBww/',
+			caption: 'Shortest path',
+			padding: 54
+		}
+	];
 });
