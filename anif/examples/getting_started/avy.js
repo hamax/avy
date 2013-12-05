19113586b58e24402ff7b99c471dd71f8a958357
Ziga Ham <ziga@hamsworld.net>
2013-12-05 05:48:42 +0100

    Apache 2.0 license

--- anif/examples/getting_started/avy.js
+++ anif/examples/getting_started/avy.js
@@ -1,3 +1,22 @@
+/* ========================================================================
+ * Avy Algorithm Visualization Framework
+ * https://github.com/hamax/avy
+ * ========================================================================
+ * Copyright 2013 Ziga Ham
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ======================================================================== */
+
 // Load required modules (core, list, node)
 define(['m/ziga/core/playback', 'm/ziga/core/svg', 'm/ziga/list/list', 'm/ziga/node/simple'],
 		function(playback, svg, list, node) {
